8c28521b9765d07f578cd118b6265c5dad60ac64
ellismckenzielee <ellismckenzielee@gmail.com>
2022-05-15 10:25:27 +0100

    add simple fun 219 solution

---
+++ simple-fun-219-light-bulbs.js
@@ -0,0 +1,22 @@
+/*
+Simple Fun 219: Lightbulbs kata
+https://www.codewars.com/kata/5901555b63bf404a66000029
+*/
+
+function lightBulbs(lights, n) {
+  let lightsCopy = [...lights];
+  for(let i = 0; i < n; i ++) {
+    lightsCopy = lightsCopy.map(switchLightBulbs)
+  }
+  return lightsCopy
+}
+
+function switchLightBulbs  (elem, indx, arr) {
+  let bulbBefore;
+  if (indx === 0) {
+    bulbBefore = arr.slice(-1)[0];
+  } else{
+    bulbBefore = arr[indx - 1]
+  }
+  return bulbBefore === 1 ? 1 - elem : elem;
+}
